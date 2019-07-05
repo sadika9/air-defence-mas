57b72a89bdfc36bc73d0fcb1b621ae20004a2714
Sadika Sumanapala <sadikahs@gmail.com>
2019-07-06 01:11:35 +0530

    Directly send coordinates from radars if HQ on target

--- radar_agent.py
+++ radar_agent.py
@@ -2,6 +2,7 @@ import asyncio
 from spade.behaviour import CyclicBehaviour
 from spade.message import Message
 from stationary_agent import StationaryAgent
+import util
 
 
 class ScanBehaviour(CyclicBehaviour):
@@ -35,15 +36,23 @@ class ScanBehaviour(CyclicBehaviour):
             self.detected = False
 
         if self.detected:
-            msg = Message(to="hq@xmpp.test")     # Instantiate the message
-            msg.set_metadata("performative", "inform")  # Set the "inform" FIPA performative
-            msg.body = '{0}|{1}|{2}|{3}|{4}'.format(self.object, str(self.object_id), self.type, str(self.at_x),
-                                                    str(self.at_y))  # Set the message content
-
-            await self.send(msg)
-            print("[RADAR] ({}) sent message to HQ".format(self.name))
+            if util.circle_contains(0, 0, 10, self.at_x, self.at_y):   # To HQ directly order missile to fire
+                await self.send_message('missile1@xmpp.test')
+                await self.send_message('missile2@xmpp.test')
+                print("[RADAR] ({}) directly sent messages missiles".format(self.name))
+            else:
+                await self.send_message("hq@xmpp.test")
+                print("[RADAR] ({}) sent message to HQ".format(self.name))
 
         await asyncio.sleep(1)
+
+    async def send_message(self, to):
+        msg = Message(to=to)  # Instantiate the message
+        msg.set_metadata("performative", "inform")  # Set the "inform" FIPA performative
+        msg.body = '{0}|{1}|{2}|{3}|{4}'.format(self.object, str(self.object_id), self.type, str(self.at_x),
+                                                str(self.at_y))  # Set the message content
+
+        await self.send(msg)
 
 
 class RadarAgent(StationaryAgent):
