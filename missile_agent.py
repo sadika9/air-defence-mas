73cbb378eaca222ee76a99fd8ac79561c1a44d7c
Sadika Sumanapala <sadikahs@gmail.com>
2019-07-06 13:27:33 +0530

    Remember destroyed items

--- missile_agent.py
+++ missile_agent.py
@@ -10,6 +10,7 @@ class RecvBehav(CyclicBehaviour):
     def __init__(self, name):
         super().__init__()
         self.name = name
+        self.destroyed_items = []
 
     async def run(self):
         msg = await self.receive(timeout=1)  # wait for a message for 10 seconds
@@ -17,7 +18,8 @@ class RecvBehav(CyclicBehaviour):
             print("[MISSILE] ({}) received message with content: {}\n".format(self.name, msg.body))
             parts = msg.body.split('|')
 
-            if parts[2] == 'enemy':
+            if parts[2] == 'enemy' and not parts[1] in self.destroyed_items:
+                self.destroyed_items.append(parts[1])
                 print("[MISSILE] ({}) Firing missile to enemy {} at ({}, {})\n".format(self.name, parts[0], parts[3], parts[4]))
 
         await asyncio.sleep(util.step_delay)
